6775d1c3528aab8cda2dfe96098be643d83f1228
team3 <team3_xzh@163.com>
2020-10-13 22:27:49 +0800

    add LeetCode20

---
+++ src/LeetCode20.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+
+/**
+ * @author Team3
+ * @date 2020/10/13 22:20
+ * <p>
+ * 20. 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ * <p>
+ * 示例 1:
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ * 输入: "{[]}"
+ * 输出: true
+ */
+public class LeetCode20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char pre = stack.pop();
+                if (pre == '(' && c != ')') {
+                    return false;
+                }
+                if (pre == '[' && c != ']') {
+                    return false;
+                }
+                if (pre == '{' && c != '}') {
+                    return false;
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
