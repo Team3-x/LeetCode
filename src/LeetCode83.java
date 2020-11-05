19d3b1bad900001a4fc0c96fab270a6a7086aba2
team3 <team3_xzh@163.com>
2020-11-05 22:28:09 +0800

    add LeetCode83

---
+++ src/LeetCode83.java
@@ -0,0 +1,34 @@
+/**
+ * @author Team3
+ * @date 2020/11/5 22:26
+ * 83. 删除排序链表中的重复元素
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ *
+ * 示例 1:
+ * 输入: 1->1->2
+ * 输出: 1->2
+ *
+ * 示例 2:
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class LeetCode83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+}
